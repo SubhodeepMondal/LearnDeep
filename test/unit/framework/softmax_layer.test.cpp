a5968cbba9ca0254c79c773ef97c54f983521ff9
subhodeep.mondal <mondaldeepsubho2017@gmail.com>
2026-09-03 10:30:02 +0530

    KAN-8 adding softmax layer validation data from numpy

---
+++ test/unit/framework/softmax_layer.test.cpp
@@ -0,0 +1,9 @@
+#include "LinearAlgebraFixtures.unit.hpp"
+#include "dense_layer_data.hpp"
+#include <LearnDeep/api/tensor.h>
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <stdfloat>
+
+Test_F(FrameworkTest, SoftmaxLayer_Test_1) {}
